2bd27329efa0df875dd6b553dd1553d49d910544
a2602619 <a2602619@gmail.com>
2022-01-30 20:01:04 +0800

    delete HelloWorld.vue, NavBar and it's fetch user's function completed.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="restaurant-forum">
-    <router-view />
+    <NavBar />
+    <main 
+      role="main"
+      class="mt-5 bg-white">
+      <router-view />
+    </main>
+    
   </div>
 </template>
 
-// <script>
+<script>
 import 'bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from './components/NavBar.vue'
 
 export default {
-  name: "App",
+  components:{
+    NavBar,
+  }
 };
 
 </script>
